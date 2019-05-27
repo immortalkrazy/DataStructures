48213bb48751913738d2fe44e15207c8efc957d4
Faisal Ibrahim <faisal.ibrahim@gmail.com>
2019-05-27 12:56:26 -0400

    new Package created

---
+++ DataStructures/src/Test/TestOutPut.java
@@ -0,0 +1,12 @@
+package Test;
+
+public class TestOutPut {
+
+      public static void main(String[] args) {
+
+            System.out.println("Hello World");
+            // TODO Auto-generated method stub
+
+      }
+
+}
