c6f3b02287e426e723e38f1e17841a31ffcc6768
Faisal Ibrahim <faisal.ibrahim@gmail.com>
2022-02-20 01:24:26 -0500

    Work on Structure class

---
+++ DataStructures/src/javaCollections/Structure.java
@@ -0,0 +1,78 @@
+/*                                                     Collection (I) 1.2
+ *                                                         |
+ *                                                         |
+ *                                                         |
+ *                 ------------------------------------------------------------------------------------
+ *                |                                        |                                          |
+ *                |                                        |                                          |
+ *              List (I)                                Set (I) 1.2                               Queue (I) 1.5
+ *              - Duplicates are Allowed                - Duplicates are NOT Allowed              - Represent objects before processing
+ *              - Insertion Order Preserved             - Insertion Order NOT Preserved           - FIFO                                                         
+ *                |                                        |                                          |
+ *                |                                        |                                          |
+ *    -------------------------                 -------------------------                 -------------------------                              
+ *    |           |           |                 |                       |                 |                       |                       
+ *    |           |           |                 |                       |                 |                       |
+ * ArrayList LinkedList    Vector 1.0        HashSet 1.2          SortedSet (I) 1.2   PriorityQueue 1.5        BlockingQueue 1.5
+ *   1.2        1.2           |                 |                       |                                         |
+ *                            |                 |                       |                                         |
+ *                          Stack 1.0     LinkedHashSet 1.4      NavigableSet (I) 1.6                             |- ProrityBlockingQueue 1.5
+ *                                                                      |                                         |
+ *                                                                      |                                         |
+ *                                                                   TreeSet 1.2                                  |- LinkedPriorityQueue 1.5
+ 
+ * 
+ * 
+ *                                                      Map (I) 1.2 P830
+ *                                                          |
+ *                                                          |                                                       Dictionary (AC) 1.0
+ *                                                          |                                                           |
+ *          -------------------------------------------------------------------------------------------------           |
+ *          |                       |                       |                       |                       |           |              
+ *          |                       |                       |                       |                       |------------                                                                                     
+ *          |                       |                       |                       |                       | 
+ *       HashMap 1.2           WeakHashMap 1.2        IdentityHashMap 1.4      SortedMap (I) 1.2       Hashtable 1.0
+ *          |                                                                       |                       |                         
+ *          |                                                                       |                       |
+ *    LinkedHashMap 1.4                                                       NavigableMap (I) 1.6     Properties 1.0
+ *                                                                                  |
+ *                                                                                  |
+ *                                                                               TreeMap 1.2     
+ *                                                                                               
+ *                                                      Sorting                                                            
+ *                                                         |
+ *                                                         |
+ *                                                         |
+ *                 ------------------------------------------------------------------------------------
+ *                |                                                                                   |
+ *                |                                                                                   |
+ *           Comparable (I)                                                                    Comparator (I)
+ *
+ *                                                     
+ *                                                      Cursors
+ *                                                         |
+ *                                                         |
+ *                                                         |
+ *                 ------------------------------------------------------------------------------------
+ *                |                                        |                                          |
+ *                |                                        |                                          |
+ *          Enumeration (I)                             Iterator (I)                            ListIterator (I)
+ *                                                                                  
+ 
+ *                                                  Utility Classes                                                            
+ *                                                         |
+ *                                                         |
+ *                                                         |
+ *                 ------------------------------------------------------------------------------------
+ *                |                                                                                   |
+ *                |                                                                                   |
+ *           Collections                                                                            Arrays
+ *                                                                                  
+ *                                                                                  
+ */
+
+package javaCollections;
+
+public class Structure {
+
+}
