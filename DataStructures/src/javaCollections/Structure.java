73b622c4d521fb1e995dad26be7bb5da12b4aeda
Faisal Ibrahim <faisal.ibrahim@gmail.com>
2022-02-24 23:56:44 -0500

    addition

--- DataStructures/src/javaCollections/Structure.java
+++ DataStructures/src/javaCollections/Structure.java
@@ -1,4 +1,8 @@
-/*                                                     Collection (I) 1.2
+/*               Clonable (I)                           Iterable (I) 
+ *                                                         |
+ *                                                         |
+ *                                                         |                                                                                                                              
+ *                                                      Collection (I) 1.2
  *                                                         |
  *                                                         |
  *                                                         |
@@ -71,8 +75,90 @@
  *                                                                                  
  */
 
+/*
+ *    Collection: 
+ *          - Represent group of individual objects as a single identity 
+ *          - root interface of collection interface 
+ *          - Methods: 
+ *                - boolean add (Object o); 
+ *                - boolean addAll(Collection c); 
+ *                - boolean remove (Object o); 
+ *                - boolean removeAll(Collection c); 
+ *                - boolean retainAll(Collection c); 
+ *                - void clear(); 
+ *                - boolean contains(Object o); 
+ *                - boolean containsAll(Collection c); 
+ *                - boolean isEmpty(); 
+ *                - int size(); 
+ *                - Object[] toArray(); 
+ *                - Iterator iterator(); 
+ *                - T[] toArray(T[] a); 
+ *                - boolean equals(Object o)
+ * 
+ *    List: 
+ *          - Duplicates are allowed 
+ *          - Insertion order is preserved 
+ *          - Differentiate duplicates by index 
+ *          - Methods: 
+ *                - boolean add (Object o); 
+ *                - boolean addAll(Collection c); 
+ *                - boolean remove (Object o); 
+ *                - boolean removeAll(Collection c); 
+ *                - boolean retainAll(Collection c); 
+ *                - void clear(); 
+ *                - boolean contains(Object o); 
+ *                - boolean containsAll(Collection c); 
+ *                - boolean isEmpty(); 
+ *                - int size(); 
+ *                - Object[] toArray(); 
+ *                - Iterator iterator(); 
+ *                - T[] toArray(T[] a); 
+ *                - boolean equals(Object o);
+ * 
+ *                - void add (int index, Object o); 
+ *                - boolean addAll(int index, Collection c);
+ *                - Object get(int index); 
+ *                - int indexOf(Object o); 
+ *                - int lastIndexOf(Object o); 
+ *                - ListIterator listIterator(); 
+ *                - ListIterator listIterator(int startIndex); 
+ *                - Object remove(int index); 
+ *                - List subList(int fromIndex, int toIndex); 
+ *                - Object set (int index, Object o);
+ *  
+ * 
+ * 
+ */
+
 package javaCollections;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Structure {
 
+      public static void main(String[] args) {
+
+            List<String> list1 = new ArrayList<>();
+            List<String> list2 = new ArrayList<>();
+
+            list1.add("Harry");
+            list1.add("Potter");
+            list1.add("James");
+            list1.add("Bond");
+            System.out.println(list1);
+
+            list2.add("1");
+            list2.add("2");
+            list2.add("3");
+            list2.add("4");
+            list2.addAll(3, list1);
+
+            System.out.println(list2);
+            System.out.println(list2.set(3, "Tom"));
+            System.out.println(list2.subList(3, 5));
+
+            list2.clear();
+            System.out.println(list2);
+      }
 }
